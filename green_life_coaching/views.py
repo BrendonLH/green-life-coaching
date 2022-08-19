cb0114b127b0f26a3c81f9f31f95d083487f3ae7
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-18 21:45:57 -0700

    linked up javascript to django added models for blogs

--- green_life_coaching/views.py
+++ green_life_coaching/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
+from green_life_coaching.models import Blog
 
-
-class Homepageview(TemplateView):
+class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class BlogListView(ListView):
+    template_name = 'blogs.html'
+    model = Blog
+    context_object_name = 'blogs'
+
+class BlogDetailView(DetailView):
+    template_name = 'blog-detail.html'
+    model = Blog
+    
